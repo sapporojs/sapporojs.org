5f14738917a55004864ca539739f8cd9e72b78ef
Ryunosuke SATO <tricknotes.rs@gmail.com>
2011-09-17 20:14:47 +0900

    [#18351937] use Capybara.configure

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,5 +2,8 @@ require 'capybara/rspec'
 require 'capybara-webkit'
 
 RSpec.configuration.include Capybara::DSL
-Capybara.default_driver = :webkit
-Capybara.app_host = ENV['APP_HOST'] || 'http://localhost:3000'
+
+Capybara.configure do |config|
+  config.default_driver = :webkit
+  config.app_host = ENV['APP_HOST'] || 'http://localhost:3000'
+end
