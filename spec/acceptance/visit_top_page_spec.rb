ce3099f21fd62f1a96ab94710a30e911a2562a94
Ryunosuke SATO <tricknotes.rs@gmail.com>
2011-09-17 22:41:24 +0900

    [#18351937] add spec for top page

--- spec/acceptance/visit_top_page_spec.rb
+++ spec/acceptance/visit_top_page_spec.rb
@@ -11,4 +11,22 @@ describe 'Visit to top page' do
       find('#header .description').should have_content('We like JavaScript.')
     end
   end
+
+  describe '#main' do
+    it 'should have entries' do
+      page.should have_css('#main .entry')
+    end
+  end
+
+  describe '#centent' do
+    it 'should have link return to top' do
+      find('#content .return a[href="#header"]').should have_content('Return to page top')
+    end
+  end
+
+  describe '#footer' do
+    it 'should have copyright' do
+      find('#footer address').should have_content('Sapporo.js')
+    end
+  end
 end
