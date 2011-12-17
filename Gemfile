9eb647ea0ada552d1cbf8fb9ddb475f8b775636b
Ryunosuke SATO <tricknotes.rs@gmail.com>
2011-12-18 01:22:29 +0900

    use capybara-webkit instead of poltergeist

--- Gemfile
+++ Gemfile
@@ -1,4 +1,4 @@
 source :rubygems
 
 gem 'rspec'
-gem 'poltergeist'
+gem 'capybara-webkit'
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,9 +1,9 @@
 require 'capybara/rspec'
-require 'capybara/poltergeist'
+require 'capybara-webkit'
 
 RSpec.configuration.include Capybara::DSL
 
 Capybara.configure do |config|
-  config.default_driver = :poltergeist
+  config.default_driver = :webkit
   config.app_host = ENV['APP_HOST'] || 'http://localhost:3000'
 end
