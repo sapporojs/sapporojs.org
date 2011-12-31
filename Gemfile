70496c83e75fc7fb757bb01e348e711166966dea
Ryunosuke SATO <tricknotes.rs@gmail.com>
2012-01-01 04:32:31 +0900

    remove unneccessary files

--- Gemfile
+++
@@ -1,4 +0,0 @@
-source :rubygems
-
-gem 'rspec'
-gem 'capybara-webkit'
--- spec/spec_helper.rb
+++
@@ -1,9 +0,0 @@
-require 'capybara/rspec'
-require 'capybara-webkit'
-
-RSpec.configuration.include Capybara::DSL
-
-Capybara.configure do |config|
-  config.default_driver = :webkit
-  config.app_host = ENV['APP_HOST'] || 'http://localhost:3000'
-end
