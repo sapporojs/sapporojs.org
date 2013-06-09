e3896f1c823d99f4f0e6b90c21e33379e840684f
Ryunosuke SATO <tricknotes.rs@gmail.com>
2013-06-09 20:13:50 +0900

    Disable routing

--- source/js/app.js
+++ source/js/app.js
@@ -4,7 +4,8 @@
 //=require_tree ./controllers
 //=require_tree ./views
 //=require_tree ./templates
-//=require routes
+//=require_tree ./routes
+//=require router
 
 Sapporojs = Ember.Application.create({
   rootElement: '#search-application'
---
+++ source/js/router.js
@@ -0,0 +1,3 @@
+Sapporojs.Router.reopen({
+  location: 'none'
+});
