68fbcb92a5c34ba390737c815000ba015859f962
Ryunosuke SATO <tricknotes.rs@gmail.com>
2013-06-11 16:32:13 +0000

    Update with ba37430

--- js/templates/search_form-01a1aa23.js
+++
@@ -1,1 +0,0 @@
-Ember.TEMPLATES["search/form"]=Ember.Handlebars.compile(["{{#view Sapporojs.SearchView}}","{{view Sapporojs.SearchTextFieldView valueBinding=controller.query}}","{{/view}}","{{view Sapporojs.SearchResultView}}"].join(""));
