d93703747233a4626520feb91b6618b1f92aae49
Ryunosuke SATO <tricknotes.rs@gmail.com>
2013-09-01 00:42:33 +0900

    Highlight match text

--- source/js/controllers/seach_result_controller.js
+++ source/js/controllers/seach_result_controller.js
@@ -9,5 +9,27 @@ Sapporojs.SearchResultController = Ember.ObjectController.extend({
     return queries.any(function(query) {
       return title.match(query) || text.match(query);
     });
-  }).property('controllers.query.content')
+  }).property('controllers.query.content'),
+
+  highlightQuery: Ember.computed(function() {
+    var summary = this.get('summary_text');
+    var queries = this.get('controllers.query.searchQueries');
+
+    var sources = queries.map(function(query) {
+      return query.source;
+    });
+
+    return new RegExp(sources.join('|'), 'ig');
+  }).property('controllers.query.content'),
+
+  highlightSummary: Ember.computed(function() {
+    var text  = this.get('summary_text');
+    var query = this.get('highlightQuery');
+
+    var highlighted = text.replace(query, function(matched) {
+      return '<strong><i>' + matched + '</i></strong>';
+    });
+
+    return Ember.String.htmlSafe(highlighted);
+  }).property('summary_text', 'controllers.query.content')
 });
