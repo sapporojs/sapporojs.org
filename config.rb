321f9ccf67270f19b4ccb7f0a59fd44b83571e95
Ryunosuke SATO <tricknotes.rs@gmail.com>
2013-11-27 23:53:41 +0900

    Cleanup config.rb using `config.environment`

--- config.rb
+++ config.rb
@@ -7,14 +7,10 @@ set :images_dir, 'images'
 Time.zone = 'Tokyo'
 
 configure :development do
-  ENV['BUILD_ENV'] ||= 'development'
-
   set :debug_assets, true
 end
 
 configure :build do
-  ENV['BUILD_ENV'] ||= 'production'
-
   activate :minify_css
   activate :minify_javascript
   activate :asset_hash
